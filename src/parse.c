cc96f59c67d662866ec7d888e7f2e6d17f2d1758
liuhaonk@gmail.com <liuhaonk@gmail.com@ff57971a-9c86-f9e2-223b-5c1975eb5164>
2011-05-10 09:54:02 +0000

    remove obsolete code used to manually input force field

--- src/parse.c
+++ src/parse.c
@@ -188,148 +188,6 @@ parse_input_global (FILE *fp, System *tpm_system)
 }
 
 void
-parse_input_bond_parameter (FILE *fp, System *tpm_system)
-{
-  int i = 0;
-  char buffer[200];
-
-  for (i=0; i<tpm_system->number_bond_type; i++)
-  {
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%lf %lf", 
-            &((tpm_system->bond_type+i)->r0), 
-            &((tpm_system->bond_type+i)->K));
-  }
-}
-
-void
-parse_input_angle_parameter (FILE *fp, System *tpm_system)
-{
-  int i = 0;
-  char buffer[200];
-
-  for (i=0; i<tpm_system->number_angle_type; i++)
-  {
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%lf %lf", 
-            &((tpm_system->angle_type+i)->theta0), 
-            &((tpm_system->angle_type+i)->K));
-  }
-}
-
-void
-parse_input_dihedral_parameter (FILE *fp, System *tpm_system)
-{
-  int i = 0;
-  char buffer[200];
-
-  for (i=0; i<tpm_system->number_dihedral_type; i++)
-  {
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%lf %lf", 
-            &((tpm_system->dihedral_type+i)->phi0), 
-            &((tpm_system->dihedral_type+i)->K));
-  }
-}
-
-void
-parse_input_improper_parameter (FILE *fp, System *tpm_system)
-{
-  int i = 0;
-  char buffer[200];
-
-  for (i=0; i<tpm_system->number_improper_type; i++)
-  {
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%lf %lf", 
-            &((tpm_system->improper_type+i)->chi0), 
-            &((tpm_system->improper_type+i)->K));
-  }
-}
-
-
-void
-parse_input_force_parameter (FILE *fp, System *tpm_system)
-{
-  char buffer[200];
-
-  fgets (buffer, 200, fp);
-  sscanf (buffer, "%d %d %d %d", 
-          &(tpm_system->number_bond_type),
-          &(tpm_system->number_angle_type),
-          &(tpm_system->number_dihedral_type),
-          &(tpm_system->number_improper_type));
-  tpm_system->bond_type     = malloc (tpm_system->number_bond_type *
-                                      sizeof (BONDTYPE));
-  tpm_system->angle_type    = malloc (tpm_system->number_angle_type *
-                                      sizeof (ANGLETYPE));
-  tpm_system->dihedral_type = malloc (tpm_system->number_dihedral_type *
-                                      sizeof (DIHEDRALTYPE));
-  tpm_system->improper_type = malloc (tpm_system->number_improper_type *
-                                      sizeof (IMPROPERTYPE));
-
-  parse_input_bond_parameter (fp, tpm_system);
-  parse_input_angle_parameter (fp, tpm_system);
-  parse_input_dihedral_parameter (fp, tpm_system);
-  parse_input_improper_parameter (fp, tpm_system);
-}
-
-void
-parse_input_atom_detail (FILE *fp, ATOM *atom, System *tpm_system)
-{
-  int i;
-  char buffer[200];
-
-  const char atom_type[ATOM_TYPE_MAX][4]=
-  {
-    "L",
-    "H","He",
-    "Li","Be","B","C","N","O","F","Ne",
-    "Na","Mg","Al","Si","P","S","Cl","Ar",
-    "K","Ca",
-    "Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn",
-    "Ga","Ge","As","Se","Br","Kr",
-    "Rb","Sr",
-    "Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd",
-    "In","Sn","Sb","Te","I","Xe",
-    "Cs","Ba",
-    "La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
-    "Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg",
-    "Tl","Pb","Bi","Po","At","Rn",
-    "Fr","Ra",
-    "Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No",
-    "Lr","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn",
-    "Uut","Uuq","Uup","Uuh","Uus","Uuo"    
-  };
-
-
-  fgets (buffer, 200, fp);
-  sscanf (buffer, "%lf %lf %lf %s %lf %lf", 
-          &(atom->x),
-          &(atom->y),
-          &(atom->z),
-          atom->symbol,
-          &(atom->mass),
-          &(atom->charge));
-          
-  for (i=0; i<ATOM_TYPE_MAX; i++)
-  {
-    if (!strcmp (atom->symbol, atom_type[i]))
-    {
-      atom->type = i;
-      break;
-    }
-  }
-  
-  atom->vx = 0.0;
-  atom->vy = 0.0;
-  atom->vz = 0.0;
-  atom->ax = 0.0;
-  atom->ay = 0.0;
-  atom->az = 0.0;
-}
-
-void
 parse_input_atom_detail_auto (FILE *fp, ATOM *atom, System *tpm_system)
 {
   int i;
@@ -417,46 +275,6 @@ parse_input_atom_detail_auto (FILE *fp, ATOM *atom, System *tpm_system)
 }
 
 void
-parse_input_atom (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
-{
-  int i;
-  
-  //Initiate Atoms and Calculate Center of Gravity
-  double accumulated_center_of_gravity_x = 0.0;
-  double accumulated_center_of_gravity_y = 0.0;
-  double accumulated_center_of_gravity_z = 0.0;
-  double accumulated_mass                = 0.0;
-  
-  for (i=0; i<mol_type->molecule.nAtoms; i++)
-  {
-    ATOM *atom = mol_type->molecule.atoms+i;
-    
-    parse_input_atom_detail (fp, atom, tpm_system);
-    
-    accumulated_center_of_gravity_x += atom->x * atom->mass;
-    accumulated_center_of_gravity_y += atom->y * atom->mass;
-    accumulated_center_of_gravity_z += atom->z * atom->mass;
-    accumulated_mass                += atom->mass;
-  }
-  double center_of_gravity_x = accumulated_center_of_gravity_x /
-                               accumulated_mass;
-  double center_of_gravity_y = accumulated_center_of_gravity_y /
-                               accumulated_mass;
-  double center_of_gravity_z = accumulated_center_of_gravity_z /
-                               accumulated_mass;
-
-  //Adjust Origin to Center of Gravity
-  for (i=0; i<mol_type->molecule.nAtoms; i++)
-  {
-    ATOM *atom = mol_type->molecule.atoms+i;
-
-    atom->x -= center_of_gravity_x;
-    atom->y -= center_of_gravity_y;
-    atom->z -= center_of_gravity_z;
-  }
-}
-
-void
 parse_input_atom_auto (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
 {
   int i;
@@ -497,27 +315,6 @@ parse_input_atom_auto (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
 }
 
 void
-parse_input_bond (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
-{
-  int i;
-  char buffer[200];
-  
-  //Initiate Bonds
-  for (i=0; i<mol_type->molecule.nBonds; i++)
-  {
-    BOND *bond = mol_type->molecule.bonds + i;
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%d %d %d %d ", 
-            &(bond->idxAtom1),
-            &(bond->idxAtom2),
-            &(bond->bondType),
-            &(bond->type));
-    (bond->idxAtom1)--;
-    (bond->idxAtom2)--;
-  }
-}
-
-void
 parse_input_bond_auto (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
 {
   int i,j;
@@ -549,29 +346,6 @@ parse_input_bond_auto (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
         bond->k2 = qb->k2;
       }
     }
-  }
-
-}
-
-void
-parse_input_angle (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
-{
-  int i;
-  char buffer[200];
-
-  //Initiate Angles
-  for(i=0;i<mol_type->molecule.nAngles;i++)
-  {
-    ANGLE *angle = mol_type->molecule.angles + i;
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%d %d %d %d", 
-            &(angle->idxAtom1),
-            &(angle->idxAtom2),
-            &(angle->idxAtom3),
-            &(angle->type));
-    (angle->idxAtom1)--;
-    (angle->idxAtom2)--;
-    (angle->idxAtom3)--;
   }
 }
 
@@ -643,31 +417,6 @@ parse_input_angle_auto (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
         angle->k2 = qa->k2;
       }
     }
-  }
-
-}
-
-void
-parse_input_dihedral (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
-{
-  int i;
-  char buffer[200];
-
-  //Initiate Dihedrals
-  for(i=0;i<mol_type->molecule.nDihedrals;i++)
-  {
-    DIHEDRAL *dihedral = mol_type->molecule.dihedrals + i;
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%d %d %d %d %d", 
-            &(dihedral->idxAtom1),
-            &(dihedral->idxAtom2),
-            &(dihedral->idxAtom3),
-            &(dihedral->idxAtom4),
-            &(dihedral->type));
-    (dihedral->idxAtom1)--;
-    (dihedral->idxAtom2)--;
-    (dihedral->idxAtom3)--;
-    (dihedral->idxAtom4)--;
   }
 }
 
@@ -1019,30 +768,6 @@ parse_input_dihedral_and_improper_auto (FILE *fp, MOLECULETYPE *mol_type, System
   }
 
 }
-void
-parse_input_improper (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
-{
-  int i;
-  char buffer[200];
-
-  //Initiate Impropers
-  for(i=0;i<mol_type->molecule.nImpropers;i++)
-  {
-    IMPROPER *improper = mol_type->molecule.impropers + i;
-    fgets (buffer, 200, fp);
-    sscanf (buffer, "%d %d %d %d %d", 
-            &(improper->idxAtom1),
-            &(improper->idxAtom2),
-            &(improper->idxAtom3),
-            &(improper->idxAtom4),
-            &(improper->type));
-    (improper->idxAtom1)--;
-    (improper->idxAtom2)--;
-    (improper->idxAtom3)--;
-    (improper->idxAtom4)--;
-  }
-}
-
 
 void
 parse_input_molecule_type (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
@@ -1070,13 +795,6 @@ parse_input_molecule_type (FILE *fp, MOLECULETYPE *mol_type, System *tpm_system)
                                          sizeof (DIHEDRAL));
   mol_type->molecule.impropers     = malloc (mol_type->molecule.nImpropers *
                                          sizeof (IMPROPER));
-/* Mannully input force field
-  parse_input_atom (fp, mol_type, tpm_system);
-  parse_input_bond (fp, mol_type, tpm_system);
-  parse_input_angle (fp, mol_type, tpm_system);
-  parse_input_dihedral (fp, mol_type, tpm_system);
-  parse_input_improper (fp, mol_type, tpm_system);
-*/
 // Automatic grenerate force field
   parse_input_atom_auto (fp, mol_type, tpm_system);
   parse_input_bond_auto (fp, mol_type, tpm_system);
@@ -1112,8 +830,6 @@ parse_input_file (FILE *fp, System *tpm_system)
   tpm_system->dimension = tpm_system->relaxed_dimension
                           * tpm_system->expansion_ratio;
   tpm_system->half_dimension = tpm_system->dimension*0.5;
-  
-  parse_input_force_parameter (fp, tpm_system);
   
   parse_input_molecule (fp, tpm_system);
 }
